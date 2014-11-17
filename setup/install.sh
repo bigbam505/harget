173383e54424b64f307ab23efdc76dfafcd5b7a2
Montague, Brent <bmontague@cvent.com>
2014-11-16 22:56:20 -0500

    Add supervisor for harget server

--- setup/install.sh
+++ setup/install.sh
@@ -13,8 +13,15 @@ if [ $ROOT_AVAIL -lt $MIN_REQ ]; then
   #exit 1 
 fi
 
+echo "Updating system package database..."
+sudo apt-get -qq update > /dev/null
+
+echo "Upgrading the system..."
+echo "(This might take a while.)"
+#sudo apt-get -y -qq upgrade > /dev/null
+
 echo "Installing system dependencies..."
-sudo apt-get -y -qq install git-core python-pip vim > /dev/null
+sudo apt-get -y -qq install git-core python-pip vim supervisor > /dev/null
 
 
 # Setup the app location
@@ -28,3 +35,7 @@ sudo pip install -r /harget/dependencies.txt -q > /dev/null
 
 sudo cp /harget/setup/config.yaml /harget/service_config.yaml
 
+echo "Setup server to run automagically at startup"
+sudo ln -s /harget/setup/supervisor_harget_server.conf /etc/supervisor/conf.d/harget_server.conf
+sudo /etc/init.d/supervisor stop > /dev/null
+sudo /etc/init.d/supervisor start > /dev/null
