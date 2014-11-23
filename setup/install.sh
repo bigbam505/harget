40f3fee3460f6b22fc9546e004a1efba76ebca46
Montague, Brent <bmontague@cvent.com>
2014-11-23 11:19:28 -0500

    Add a basic viewer to display websites through uzbl

--- setup/install.sh
+++ setup/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo 'Installing some shit'
 
 ## Simple disk storage check. Naively assumes root partition holds all system data.
@@ -20,24 +19,61 @@ echo "(This might take a while.)"
 #sudo apt-get -y -qq upgrade > /dev/null
 
 echo "Installing system dependencies..."
-sudo apt-get -y -qq install git-core python-pip vim supervisor > /dev/null
+sudo apt-get -y -qq install git-core python-pip vim supervisor uzbl > /dev/null
 
 
 # Setup the app location
-mkdir ~/harget > /dev/null
-chown pi ~/harget > /dev/null
-git clone https://github.com/bigbam505/harget.git ~/harget > /dev/null
-
+install_directory="$HOME/harget"
+if [ ! -d "$install_directory" ]; then
+  mkdir "$install_directory"  > /dev/null
+  chown pi:pi "$install_directory" > /dev/null
+  git clone https://github.com/bigbam505/harget.git "$install_directory" > /dev/null
+fi
 
 echo "Installing more dependencies..."
-sudo pip install -r ~/harget/dependencies.txt -q > /dev/null
+sudo pip install -r "$install_directory/dependencies.txt" -q > /dev/null
 
-cp ~/harget/setup/config.yaml ~/harget/service.yaml
+cp "$install_directory/setup/config.yaml" "$install_directory/service.yaml"
 
-sudo mkdir /var/log/harget-server
-sudo chown pi:pi /var/log/harget-server
+echo "Setup the logging files"
+harget_log="/var/log/harget"
+if [ ! -d "$harget_log" ]; then
+  sudo mkdir "$harget_log"
+fi
+sudo chown pi:pi "$harget_log"
+
+# Lets create some log folders
+if [ ! -d "$harget_log/server" ]; then
+  mkdir "$harget_log/server"
+fi
+
+if [ ! -d "$harget_log/viewer" ]; then
+  mkdir "$harget_log/viewer"
+fi
+
+echo "Setup the html folder in tmp"
+harget_files="/var/harget"
+if [ ! -d "$harget_files" ]; then
+  sudo mkdir "$harget_files"
+fi
+
+sudo chown pi:pi "$harget_files"
+ln -sf "$install_directory/html" "$harget_files/html"
+
+echo "Setup some configuration files for the viewer"
+ln -sf "$install_directory/setup/uzbl.rc" "$HOME/.uzbl.rc"
 
 echo "Setup server to run automagically at startup"
-sudo ln -s ~/harget/setup/supervisor_harget_server.conf /etc/supervisor/conf.d/harget_server.conf
+sudo ln -sf "$install_directory/setup/supervisor_harget_server.conf" /etc/supervisor/conf.d/harget_server.conf
 sudo /etc/init.d/supervisor stop > /dev/null
 sudo /etc/init.d/supervisor start > /dev/null
+
+echo "Setup viewer to run automagically at startup"
+mkdir -p ~/.config/lxsession/LXDE/
+echo "@$install_directory/setup/autostart.sh" > ~/.config/lxsession/LXDE/autostart
+
+if [-d /etc/xdg/lxsession/LXDE/autostart ]; then
+  echo 'test'
+  #sudo mv /etc/xdg/lxsession/LXDE/autostart /etc/xdg/lxsession/LXDE/autostart.bak
+fi
+
---
+++ viewer.py
@@ -0,0 +1,131 @@
+import sh
+import sys
+import json
+import yaml
+import logging
+import os.path
+from time import sleep, time
+from os import path, getenv
+from signal import signal, SIGUSR1, SIGUSR2
+
+BLANK_PAGE = "file:///var/harget/html/blank.html"
+SLEEP_TIME = 2
+current_browser_url = None
+browser = None
+config_file = 'service.yaml'
+HOME = getenv("HOME","/home/pi")
+config = None
+
+def dummy_function(signum, frame):
+  """this is stupid"""
+
+def json_parse(obj):
+  return json.loads(obj)
+
+def set_logging_level():
+  logging.getLogger().setLevel(logging.DEBUG)
+
+def load_config():
+  global config
+  if(os.path.isfile(config_file)):
+    f = open(config_file)
+    config = yaml.safe_load(f)
+    f.close()
+
+def browser_send(command, cb=lambda _: True):
+  if not (browser is None) and browser.process.alive:
+    while not browser.process._pipe_queue.empty():  # flush stdout
+      browser.next()
+
+  browser.process.stdin.put(command + '\n')
+  while True:  # loop until cb returns True
+    if cb(browser.next()):
+      break
+    else:
+      logging.info('browser found dead, restarting')
+      load_browser()
+
+def load_browser(url=None):
+  global browser, current_browser_url
+  logging.info('Loading browser...')
+
+  if browser:
+    logging.info('killing previous uzbl %s', browser.pid)
+    browser.process.kill()
+
+  if not url is None:
+    current_browser_url = url
+
+    # --config=-       read commands (and config) from stdin
+    # --print-events   print events to stdout
+    browser = sh.Command('uzbl-browser')(print_events=True, config='-', uri=current_browser_url, _bg=True)
+    logging.info('Browser loading %s. Running as PID %s.', current_browser_url, browser.pid)
+
+    uzbl_rc = 'set ssl_verify = {}\n'.format('1')
+    with open(HOME + "/.uzbl.rc") as f:  # load uzbl.rc
+      uzbl_rc = f.read() + uzbl_rc
+    browser_send(uzbl_rc)
+
+def update_browser_url(url, cb=lambda _: True):
+  browser_send('uri ' + url, cb=cb)
+
+def change_url(url):
+  global current_browser_url
+  current_browser_url = url
+  logging.debug('Changing to ' + url)
+
+  return url
+
+def get_next_url():
+  url_object = None
+  url_file_path = config['url_location']
+  if(not os.path.isdir(os.path.dirname(url_file_path))):
+    os.makedirs(os.path.dirname(url_file_path))
+
+  if(os.path.isfile(url_file_path)):
+    f = open(url_file_path, 'r')
+    url_object = json_parse(f.read())
+
+  if(url_object != None and url_object['url'] != ""):
+    return url_object['url']
+
+  return BLANK_PAGE
+
+def screen_refresh_loop():
+  global current_browser_url
+  next_url = get_next_url()
+
+  if(next_url !=  current_browser_url):
+    update_browser_url(change_url(next_url))
+
+  sleep(SLEEP_TIME)
+
+def setup():
+  logging.debug('Lets setup first')
+  load_config()
+  set_logging_level()
+
+  signal(SIGUSR1, dummy_function)
+  #signal(SIGUSR2, dummy_function)
+
+def main(argv):
+  global config_file
+
+  if(len(argv) > 0):
+    config_file = argv[0]
+
+  setup()
+
+  if(not config or config == None):
+    logging.error('Not config loaded, please add a config file')
+    logging.error('Usage: python viewer.py service.yaml')
+    return
+
+  logging.debug('Woah It worked')
+
+  load_browser(url=BLANK_PAGE)
+  while True:
+    screen_refresh_loop()
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
